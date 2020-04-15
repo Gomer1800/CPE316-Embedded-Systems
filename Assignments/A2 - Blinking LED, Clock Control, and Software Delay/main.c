9f4e90126227b647224ba66797509adae53e9e35
Luis Gomez <luisgomez1800@gmail.com>
2020-04-15 15:07:55 -0700

    Fixed comments, added a TODO for delay function

--- Assignments/A2 - Blinking LED, Clock Control, and Software Delay/main.c
+++ Assignments/A2 - Blinking LED, Clock Control, and Software Delay/main.c
@@ -24,13 +24,13 @@ void main(void)
     setup_RED_LED();
 
     while(1) {
-        uint32_t DELAY1S = 10000000;
+        uint32_t DELAY10S = 10000000; // 10 seconds
 
         P1->OUT |= (BIT0);          /* turn on  P1.0 red LED */
-        delay_us(DELAY1S);
+        delay_us(DELAY10S);
 
         P1->OUT &= ~(BIT0);         /* turn off P1.0 red LED */
-        delay_us(DELAY1S);
+        delay_us(DELAY10S);
     }
 }
 
@@ -42,6 +42,7 @@ void delay_us(uint32_t microseconds) {
     // Calculate number of iterations for desired delay
     uint32_t num_loops;
 
+    // TODO Luis: Figure out how an exact expression for number of us per loop, current one overshoots
     switch(FREQ)
     {
         case FREQ_3MHZ:
@@ -57,7 +58,7 @@ void delay_us(uint32_t microseconds) {
             num_loops = (microseconds * 24)/7;
             break;
         default:
-            num_loops = (microseconds * 1.5)/7;
+            num_loops = microseconds * 1.5)/7;
             break;
     }
 
