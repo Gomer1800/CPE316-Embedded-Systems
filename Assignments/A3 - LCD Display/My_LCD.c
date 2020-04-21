a6a8a3c53ceb1b01a8a3b32dc0d883afba40b191
Luis Gomez <luisgomez1800@gmail.com>
2020-04-21 11:22:35 -0700

    Creating skeleton of lcd header

---
+++ Assignments/A3 - LCD Display/My_LCD.c
@@ -0,0 +1,9 @@
+#include "My_LCD.h"
+/*
+ * My_LCD.c
+ *
+ *  Created on: Apr 21, 2020
+ *      Author: Luis Gomez
+ */
+
+
---
+++ Assignments/A3 - LCD Display/My_LCD.h
@@ -0,0 +1,14 @@
+/*
+ * My_LCD.h
+ *
+ *  Created on: Apr 21, 2020
+ *      Author: Luis Gomez
+ */
+
+#ifndef MY_LCD_H_
+#define MY_LCD_H_
+
+#include "msp.h"
+#include  "STDINT.H" // definition of uint32_t
+
+#endif /* MY_LCD_H_ */
