2f60a83a167a44abd832e6ff967195226b1ff8d1
Luis Gomez <luisgomez1800@gmail.com>
2020-06-11 19:14:04 -0700

    A8 - UART communications (#12)
    * UART TX function and init created
    
    * New initialization sequence, used ch 24 in TRM to set registers correctly
    
    * UART Transmissions works
    
    * Stuck, TX works but not RX
    
    * Complete

--- Assignments/A5 - Interrupts and Timers/Debug/My_Timer.d
+++ Assignments/A5 - Interrupts and Timers/Debug/My_Timer.d
@@ -2,42 +2,42 @@
 
 My_Timer.obj: ../My_Timer.c
 My_Timer.obj: ../My_Timer.h
-My_Timer.obj: C:/Users/luisg/ti/ccs1000/ccs/ccs_base/arm/include/msp.h
-My_Timer.obj: C:/Users/luisg/ti/ccs1000/ccs/ccs_base/arm/include/msp432p401r.h
-My_Timer.obj: C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdint.h
-My_Timer.obj: C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/_stdint40.h
-My_Timer.obj: C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/stdint.h
-My_Timer.obj: C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/_ti_config.h
-My_Timer.obj: C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/linkage.h
-My_Timer.obj: C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/cdefs.h
-My_Timer.obj: C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/_types.h
-My_Timer.obj: C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_types.h
-My_Timer.obj: C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_stdint.h
-My_Timer.obj: C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/_stdint.h
-My_Timer.obj: C:/Users/luisg/ti/ccs1000/ccs/ccs_base/arm/include/msp_compatibility.h
-My_Timer.obj: C:/Users/luisg/ti/ccs1000/ccs/ccs_base/arm/include/msp432p401r_classic.h
-My_Timer.obj: C:/Users/luisg/ti/ccs1000/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
-My_Timer.obj: C:/Users/luisg/ti/ccs1000/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
-My_Timer.obj: C:/Users/luisg/ti/ccs1000/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
-My_Timer.obj: C:/Users/luisg/ti/ccs1000/ccs/ccs_base/arm/include/system_msp432p401r.h
+My_Timer.obj: C:/ti/ccs1000/ccs/ccs_base/arm/include/msp.h
+My_Timer.obj: C:/ti/ccs1000/ccs/ccs_base/arm/include/msp432p401r.h
+My_Timer.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdint.h
+My_Timer.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/_stdint40.h
+My_Timer.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/stdint.h
+My_Timer.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/_ti_config.h
+My_Timer.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/linkage.h
+My_Timer.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/cdefs.h
+My_Timer.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/_types.h
+My_Timer.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_types.h
+My_Timer.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_stdint.h
+My_Timer.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/_stdint.h
+My_Timer.obj: C:/ti/ccs1000/ccs/ccs_base/arm/include/msp_compatibility.h
+My_Timer.obj: C:/ti/ccs1000/ccs/ccs_base/arm/include/msp432p401r_classic.h
+My_Timer.obj: C:/ti/ccs1000/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
+My_Timer.obj: C:/ti/ccs1000/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+My_Timer.obj: C:/ti/ccs1000/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+My_Timer.obj: C:/ti/ccs1000/ccs/ccs_base/arm/include/system_msp432p401r.h
 
 ../My_Timer.c: 
 ../My_Timer.h: 
-C:/Users/luisg/ti/ccs1000/ccs/ccs_base/arm/include/msp.h: 
-C:/Users/luisg/ti/ccs1000/ccs/ccs_base/arm/include/msp432p401r.h: 
-C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdint.h: 
-C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/_stdint40.h: 
-C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/stdint.h: 
-C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/_ti_config.h: 
-C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/linkage.h: 
-C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/cdefs.h: 
-C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/_types.h: 
-C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_types.h: 
-C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_stdint.h: 
-C:/Users/luisg/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/_stdint.h: 
-C:/Users/luisg/ti/ccs1000/ccs/ccs_base/arm/include/msp_compatibility.h: 
-C:/Users/luisg/ti/ccs1000/ccs/ccs_base/arm/include/msp432p401r_classic.h: 
-C:/Users/luisg/ti/ccs1000/ccs/ccs_base/arm/include/CMSIS/core_cm4.h: 
-C:/Users/luisg/ti/ccs1000/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
-C:/Users/luisg/ti/ccs1000/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
-C:/Users/luisg/ti/ccs1000/ccs/ccs_base/arm/include/system_msp432p401r.h: 
+C:/ti/ccs1000/ccs/ccs_base/arm/include/msp.h: 
+C:/ti/ccs1000/ccs/ccs_base/arm/include/msp432p401r.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdint.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/_stdint40.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/stdint.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/_ti_config.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/linkage.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/cdefs.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/_types.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_types.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_stdint.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/_stdint.h: 
+C:/ti/ccs1000/ccs/ccs_base/arm/include/msp_compatibility.h: 
+C:/ti/ccs1000/ccs/ccs_base/arm/include/msp432p401r_classic.h: 
+C:/ti/ccs1000/ccs/ccs_base/arm/include/CMSIS/core_cm4.h: 
+C:/ti/ccs1000/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
+C:/ti/ccs1000/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+C:/ti/ccs1000/ccs/ccs_base/arm/include/system_msp432p401r.h: 
