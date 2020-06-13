0bd67582ccdc9a81efe1ddbc7adc6b048f31eef2
Luis Gomez <luisgomez1800@gmail.com>
2020-06-13 11:29:28 -0700

    Complete

--- Assignments/Exam3-Q4/main.c
+++ Assignments/Exam3-Q4/main.c
@@ -5,14 +5,22 @@
 void setup_ADC14(void);
 void __delay_ms(uint8_t delay);
 
-uint16_t digitalVal;
-uint8_t  flag = 1; // ADC ready flag
+uint16_t digitalVal;    // used for 14 bit ADC data
+uint8_t  flag = 1;      // ADC ready flag
 
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 
-    // Configure GPIO
+    // Configure GPIO LED0 P1.1
+    P1->DIR  |=  BIT1;
+    P1->SEL1 &= ~BIT1;
+    P1->SEL0 &= ~BIT1;
+    // Configure GPIO LED1 P1.2
+    P1->DIR  |=  BIT2;
+    P1->SEL1 &= ~BIT2;
+    P1->SEL0 &= ~BIT2;
+    // Configure GPIO ADC P4.7
     P4->SEL1 |= BIT7; //Set to alternate mode| BIT6 | BIT7;
     P4->SEL0 |= BIT7; //P4.7 is A/D channel A6 according to quick guide
 
@@ -28,9 +36,20 @@ void main(void)
         __delay_ms(_250MS);
         __delay_ms(_250MS);
         __delay_ms(_250MS);
-        ADC14->CTL0 |= ADC14_CTL0_SC;               // Start conversion-software trigger
-        while(flag == 0){}
-
+        ADC14->CTL0 |= ADC14_CTL0_SC;   // Start conversion-software trigger
+        while(flag == 0){}              // waiting for ADC conversion to be ready
+        if(digitalVal < 3277) // less than 20%
+        {
+            P1->OUT &= ~BIT1;
+            P1->OUT |=  BIT2;
+        }
+        if(digitalVal < 13107)// less than 80%
+            P1->OUT |= (BIT1 | BIT2);
+        else // else{
+        {
+            P1->OUT |=  BIT1;
+            P1->OUT &= ~BIT2;
+        }
         flag = 0;
     }
 }
